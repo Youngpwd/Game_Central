d2a72a45aebb6779e57b0cb2a79f100cfcd31c86
Charles <cbyoungx97@gmail.com>
2022-03-02 10:35:22 -0800

    add signup form

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,18 @@
-// topnav
+// topnav var
 const navVar = document.getElementById("myTopnav");
+let navBars = document.getElementById("icon");
 
+// login var
+let modalLogin = document.getElementById("id01");
+let login = document.getElementById("login");
+
+// Sign Up var
+let modalSign = document.getElementById("id02");
+let sign = document.getElementById("sign-up");
+let closeIcon = document.getElementById("close-icon-su");
+let cancelBtn2 = document.getElementById("cancelbtn-2");
+
+// topnav function
 function navbar() {
   let x = navVar;
   if (x.className === "topnav") {
@@ -10,29 +22,41 @@ function navbar() {
   }
 }
 
-// login
-let modalLogin = document.getElementById("id01");
-
-let login = document.getElementById("login");
-
+//login function
 function showModal() {
   modalLogin.style.display = "block";
-}
-
+};
 function hideModal() {
   modalLogin.style.display = "none";
-}
+};
 
+// sign up function
+function showModalSu() {
+  modalSign.style.display = "block";
+};
+function hideModalSu() {
+  modalSign.style.display = "none";
+};
+
+// topnav events
+navBars.addEventListener("click", navbar);
+
+// login events
 login.addEventListener("click", showModal);
-
 document.getElementById("close-icon").addEventListener("click", hideModal);
-
 document.getElementById("cancelbtn").addEventListener("click", hideModal);
-
 window.addEventListener("click", function (event) {
   if (event.target == modalLogin) {
     modalLogin.style.display = "none";
   }
 });
 
-// Sign Up
+// sign up events
+sign.addEventListener("click", showModalSu);
+closeIcon.addEventListener("click", hideModalSu);
+cancelBtn2.addEventListener("click", hideModalSu);
+window.addEventListener("click", function (event) {
+  if (event.target == modalSign) {
+    modalSign.style.display = "none";
+  }
+});
